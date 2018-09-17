142d14edaa9bd98c39f55c4d0e3422a28c6a31e9
zyotchatmajo <39542528+zyotchatmajo@users.noreply.github.com>
2018-09-16 23:02:49 -0500

    Add files via upload

---
+++ Operadores/Operadores/Comparativos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores
+{
+    class Comparativos
+    {
+        public void Com(int a, int b)
+        {
+            Console.WriteLine("Son iguales?");
+            if(a == b)
+            {
+                Console.WriteLine("Si");
+            }
+            else
+            {
+                Console.WriteLine("Son diferentes?");
+                if (a != b)
+                {
+                    Console.WriteLine("Si");
+                }
+            }
+            
+        }
+    }
+}
