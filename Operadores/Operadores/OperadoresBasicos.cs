5325dd6c5e50d12b6d28d5d79d72a034eb0cecab
zyotchatmajo <39542528+zyotchatmajo@users.noreply.github.com>
2018-09-16 22:58:08 -0500

    Add files via upload

---
+++ Operadores/Operadores/OperadoresBasicos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores
+{
+    class OperadoresBasicos
+    {
+        public void Operadores(int a, int b)
+        {
+            Console.WriteLine("a + b = " + (a + b));
+            Console.WriteLine("a - b = " + (a - b));
+            Console.WriteLine("a * b = " + (a * b));
+            Console.WriteLine("a / b = " + (a / b));
+            Console.WriteLine("a % b = " + (a % b));
+        }
+    }
+}
---
+++ Operadores/Operadores/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var OB = new OperadoresBasicos();
+            var CMP = new Comparativos();
+            Boolean salir = false;
+            while(salir == false)
+            {
+                Console.WriteLine("Introduce el primer valor");
+                int a = Convert.ToInt16(Console.ReadLine()); ;
+                Console.WriteLine("Introduce el segundo valor");
+                int b = Convert.ToInt16(Console.ReadLine()); ;
+                OB.Operadores(a, b);
+                CMP.Com(a, b);
+            }
+        }
+    }
+}
