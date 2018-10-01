f280ffe8a4423250e3655fb26562488174101849
zyotchatmajo <39542528+zyotchatmajo@users.noreply.github.com>
2018-09-30 19:37:32 -0500

    Add files via upload

---
+++ TallerTareas/Tarea#4 Palindromos/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Palindromo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Verificar = new System.Windows.Forms.Button();
+            this.textCampo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(349, 292);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 0;
+            // 
+            // Verificar
+            // 
+            this.Verificar.Location = new System.Drawing.Point(308, 351);
+            this.Verificar.Name = "Verificar";
+            this.Verificar.Size = new System.Drawing.Size(127, 23);
+            this.Verificar.TabIndex = 1;
+            this.Verificar.Text = "¿Es un palindromo?";
+            this.Verificar.UseVisualStyleBackColor = true;
+            this.Verificar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textCampo
+            // 
+            this.textCampo.Location = new System.Drawing.Point(245, 257);
+            this.textCampo.Name = "textCampo";
+            this.textCampo.Size = new System.Drawing.Size(266, 20);
+            this.textCampo.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(242, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(283, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Selecccione alguna frase predefinida o intruce una nueva.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Location = new System.Drawing.Point(305, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "¿Acaso hubo búhos acá?";
+            this.label3.UseMnemonic = false;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label4.Location = new System.Drawing.Point(306, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "A la catalana banal, atácala.";
+            this.label4.UseMnemonic = false;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label5.Location = new System.Drawing.Point(199, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(391, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "A mamá, Roma le aviva el amor a papá y a papá, Roma le aviva el amor a Mamá.";
+            this.label5.UseMnemonic = false;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label6.Location = new System.Drawing.Point(331, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "A ti no, bonita.";
+            this.label6.UseMnemonic = false;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label7.Location = new System.Drawing.Point(306, 156);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(121, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Amo la pacífica paloma.";
+            this.label7.UseMnemonic = false;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label8.Location = new System.Drawing.Point(331, 183);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(74, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Amor a Roma.";
+            this.label8.UseMnemonic = false;
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label9.Location = new System.Drawing.Point(330, 214);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(75, 13);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Ana lava lana.";
+            this.label9.UseMnemonic = false;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textCampo);
+            this.Controls.Add(this.Verificar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Palindromo Programa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Verificar;
+        private System.Windows.Forms.TextBox textCampo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ TallerTareas/Tarea#4 Palindromos/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Palindromo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public static string RemoveSpecialCharacters(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if ((str[i] >= 'A' && str[i] <= 'z'))
+                {
+                    sb.Append(str[i]);
+                }
+            }
+
+            return sb.ToString();
+        }
+        public static string RemoveAccents(string s)
+        {
+            Encoding destEncoding = Encoding.GetEncoding("iso-8859-8");
+
+            return destEncoding.GetString(
+                Encoding.Convert(Encoding.UTF8, destEncoding, Encoding.UTF8.GetBytes(s)));
+        }
+
+       
+        String[] Predefinidos = new string[] {
+        "¿Acaso hubo búhos acá?",
+        "A la catalana banal, atácala.",
+        "A mamá, Roma le aviva el amor a papá y a papá, Roma le aviva el amor a Mamá.",
+        "A ti no, bonita.",
+        "Amo la pacífica paloma.",
+        "Amor a Roma.",
+        "Ana lava lana."
+                };
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String a = textCampo.Text;
+            String CadenaInvertida = Reverse(RemoveSpecialCharacters(RemoveAccents(a))).ToLower();
+            String CadenaOriginal = RemoveSpecialCharacters(RemoveAccents(a)).ToLower();
+            if (CadenaInvertida.Equals(CadenaOriginal))
+            {
+                label1.Text = "La cadena es un palindromo";
+            }
+            else
+            {
+                label1.Text = "La cadena no es un palindromo";
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            textCampo.Text = Predefinidos[0];
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            textCampo.Text = Predefinidos[1];
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            textCampo.Text = Predefinidos[2];
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            textCampo.Text = Predefinidos[3];
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            textCampo.Text = Predefinidos[4];
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            textCampo.Text = Predefinidos[5];
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            textCampo.Text = Predefinidos[6];
+        }
+    }
+}
