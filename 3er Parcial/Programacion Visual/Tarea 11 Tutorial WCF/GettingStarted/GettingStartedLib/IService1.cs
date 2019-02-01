dff377c7cece85df4e2230d3ab16e1c599f97b8a
zyotchatmajo <39542528+zyotchatmajo@users.noreply.github.com>
2019-01-31 20:15:32 -0600

    Add files via upload

---
+++ 3er Parcial/Programacion Visual/Tarea 11 Tutorial WCF/GettingStarted/GettingStartedLib/IService1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace GettingStartedLib
+{
+    [ServiceContract(Namespace = "http://Microsoft.ServiceModel.Samples")]
+    public interface ICalculator
+    {
+        [OperationContract]
+        double Add(double n1, double n2);
+        [OperationContract]
+        double Subtract(double n1, double n2);
+        [OperationContract]
+        double Multiply(double n1, double n2);
+        [OperationContract]
+        double Divide(double n1, double n2);
+    }
+}
